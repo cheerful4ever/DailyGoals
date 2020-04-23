ba9691af4a3ba6a795e8e7fa7313ade68f8283b3
18016375 <18016375@myrp.edu.sg>
2020-04-23 18:22:43 +0800

    activities 1-1000000

--- app/src/main/java/com/example/dailygoals/MainActivity.java
+++ app/src/main/java/com/example/dailygoals/MainActivity.java
@@ -2,7 +2,14 @@ package com.example.dailygoals;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.ButtonOk);
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View arg0) {
+                RadioGroup rg = (RadioGroup) findViewById(R.id.radioGroup1);
+                int selectedButtonId = rg.getCheckedRadioButtonId();
+                RadioButton rb = (RadioButton) findViewById(selectedButtonId);
+
+                RadioGroup rg2 = (RadioGroup) findViewById(R.id.radioGroup2);
+                int selectedButtonId2 = rg2.getCheckedRadioButtonId();
+                RadioButton rb2 = (RadioButton) findViewById(selectedButtonId2);
+
+                RadioGroup rg3 = (RadioGroup) findViewById(R.id.radioGroup3);
+                int selectedButtonId3 = rg3.getCheckedRadioButtonId();
+                RadioButton rb3 = (RadioButton) findViewById(selectedButtonId3);
+
+                EditText etReflect = (EditText) findViewById(R.id.reflections);
+                String[] info = {rb.getText().toString(), rb2.getText().toString(),
+                        rb3.getText().toString(), etReflect.getText().toString()};
+                Intent i = new Intent(MainActivity.this, dailyGrade_summary.class);
+                i.putExtra("info", info);
+                startActivity(i);
+            }});
     }
+
+
 }
---
+++ app/src/main/java/com/example/dailygoals/dailyGrade_summary.java
@@ -0,0 +1,28 @@
+package com.example.dailygoals;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class dailyGrade_summary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily_grade_summary);
+
+        // Get the intent so as to get the "things" inside the intent
+        Intent i = getIntent();
+        // Get the String array named "info" we passed in
+        String[] info = i.getStringArrayExtra("info");
+        // Get the TextView object
+        TextView tv1 = findViewById(R.id.summary);
+        // Display the info on the TextView
+        tv1.setText("Read up on materials before class: " + info[0] +
+                "\nArrive on time so as not to miss important part of the lesson: " + info[1] +
+                "\nAttempt the problem myself: " + info[2] +
+                "\nReflection: " + info[3]);
+    }
+}
